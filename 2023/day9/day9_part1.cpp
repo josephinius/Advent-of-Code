9910b9930ba3af6f073b49cd17f13dfd8a44f828
Jozef Genzor <jozef.genzor@gmail.com>
2023-12-16 15:08:32 +0800

    Add files via upload

---
+++ 2023/day9/day9_part1.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cctype>
+#include <unordered_map>
+#include <limits>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+
+int get_int(const string& s, int i) {
+    while (i < s.size() && isdigit(s[i])) {
+        i++;
+    }
+    return i;
+}
+
+
+bool all_zeros(const vector<int>& vec) {
+    for (auto& x : vec) {
+        if (x != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+int main() {
+    ifstream f("input.txt");
+    string s;
+    vector<vector<int>> histories;
+    while (getline(f, s)) {
+        vector<int> history;
+        int i = 0;
+        while (i < s.size()) {
+            bool is_negative = false;
+            if (s[i] == '-') {
+                is_negative = true;
+                i++;
+            }
+            int j = get_int(s, i);
+            int n = stoi(s.substr(i, j - i));
+            if (is_negative) {
+                n *= - 1;
+            }
+            history.push_back(n);
+            cout << n << ", ";
+            i = j + 1;
+        }
+        histories.push_back(history);
+        cout << endl;
+    }
+    f.close();
+    
+    long long result = 0;
+    
+    for (auto& his: histories) {
+        vector<vector<int>> diffs;
+        diffs.push_back(his);
+        while (!all_zeros(diffs.back())) {
+            vector<int> diff;
+            for (int i = 1; i < diffs.back().size(); i++) {
+                diff.push_back(diffs.back()[i] - diffs.back()[i - 1]);
+            }
+            diffs.push_back(diff);
+        }
+        
+        /*
+        for (auto vec: diffs) {
+            for (auto x : vec) {
+                cout << x << " ";
+            }
+            cout << endl;
+        }
+         */
+        
+        int pred = 0;
+        for (int i = diffs.size() - 1; i >= 0; i--) {
+            pred += diffs[i].back();
+        }
+        cout << "pred: " << pred << endl;
+        result += pred;
+        
+    }
+    
+    cout << "Result: " << result << endl;
+}
